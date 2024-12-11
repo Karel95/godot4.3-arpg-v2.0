7ffa4b164415b6380b256b2ef403a721f498e5bc
karel95 <karelh2207@gmail.com>
2024-12-11 19:57:57 -0300

    actualizando

--- time/time_system.gd
+++ time/time_system.gd
@@ -4,7 +4,10 @@ class_name TimeSystem extends Node
 signal updated
 
 @export var date_time: DateTime
-@export var ticks_per_second: int = 6
+@export var ticks_per_sec_index: int = 1
+@export var ticks_per_sec_options: Array[int] = [2, 4, 8, 16, 32, 64, 128, 256]
+
+var is_paused: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -13,5 +16,24 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	date_time.increase_by_sec(delta * ticks_per_second)
+	if !OS.is_debug_build():
+		return
+	
+	handle_input()
+	
+	if is_paused:
+		return
+	
+	date_time.increase_by_sec(delta * ticks_per_sec_options[ticks_per_sec_index])
 	updated.emit(date_time)
+
+func handle_input() -> void:
+	if Input.is_action_just_pressed("inc_speed"):
+		ticks_per_sec_index += 1
+	if Input.is_action_just_pressed("dec_speed"):
+		ticks_per_sec_index -= 1
+	if Input.is_action_just_pressed("pause_time"):
+		is_paused = !is_paused
+		
+	ticks_per_sec_index = clamp(ticks_per_sec_index, 0, ticks_per_sec_options.size() - 1)
+	
